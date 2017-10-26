af65c444fecebcb10aa2e800895409433d6239ee
Shawn Roske <sroske@gmail.com>
2017-10-26 16:30:07 -0700

    adding startup batch file

---
+++ STARTME.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\space150\Desktop\Controller"
+start .\BodyBasics-WPF.exe
+cd "C:\Users\space150\Desktop\WindowsNoEditor"
+start .\BloodWallDisplay.exe -game -WinX=1920 -WinY=0 -SAVEWINPOS=1 -FULLSCREEN
+exit
